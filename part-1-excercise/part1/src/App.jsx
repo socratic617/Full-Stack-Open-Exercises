973797a392354a467ebef499bba02c7bc98fea09
socratic617 <erik.solis02135@gmail.com>
2024-01-29 00:37:48 -0500

    using props with components

--- part-1-excercise/part1/src/App.jsx
+++ part-1-excercise/part1/src/App.jsx
@@ -1,15 +1,27 @@
+const Hello = (props) => {
+
+  console.log(props)
+  return (
+    <div>
+      <p>
+
+        Hello {props.name}, you are {props.age} years old
+      </p>
+    </div>
+  )
+}
+
 const App = () => {
-  const now = new Date()
-  const a = 10
-  const b = 20
-  console.log(now, a + b)
+
+  const nimi = 'Pekka'
+  const ika = 10
 
   return (
     <div>
-      <p>Hello world, it is {now.toString()}</p>
-      <p>
-        {a} plus {b} is {a + b}
-      </p>
+      <h1>Greetings</h1>
+
+      <Hello name="Maya" age={26 + 10} />
+      <Hello name={nimi} age={ika} />
     </div>
   )
 }
