7a838229efdda68b9869e16d9bd3169e084e5926
socratic617 <erik.solis02135@gmail.com>
2024-02-08 17:29:31 -0500

    Problem Excercise 1.5 Javascript objects

--- part-1-excercise/part1/src/App.jsx
+++ part-1-excercise/part1/src/App.jsx
@@ -1,28 +1,32 @@
-const App = () => {
-  const course = 'Half Stack application development'
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10
-  }
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7
-  }
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14
-  }
+//Excercise 1.3
 
-  return (
-    <div>
-      <h1>{course}</h1>
-      <p>{part1.name} {part1.exercises}</p>
-      <p>{part2.name} {part2.exercises}</p>
-      <p>{part3.name} {part3.exercises}</p> 
-    </div>
-  )
-}
-export default App
+// const App = () => {
+//   const course = 'Half Stack application development'
+//   const part1 = {
+//     name: 'Fundamentals of React',
+//     exercises: 10
+//   }
+//   const part2 = {
+//     name: 'Using props to pass data',
+//     exercises: 7
+//   }
+//   const part3 = {
+//     name: 'State of a component',
+//     exercises: 14
+//   }
+
+//   return (
+//     <div>
+//       <h1>{course}</h1>
+//       <p>{part1.name} {part1.exercises}</p>
+//       <p>{part2.name} {part2.exercises}</p>
+//       <p>{part3.name} {part3.exercises}</p> 
+//     </div>
+//   )
+// }
+// export default App
+
+//Excercise 1.4
 
 // const App = () => {
 //   const course = 'Half Stack application development'
@@ -51,3 +55,24 @@ export default App
 // }
 
 // export default App
+
+//Excercise 1.5 Chapter 1
+
+const App = () => {
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
