f9fdc4253a72ea085eca9788efc10579eb1eca56
socratic617 <erik.solis02135@gmail.com>
2024-02-11 18:54:15 -0500

    added event handlers when buttons clicked

--- part-1-excercise/part1/src/App.jsx
+++ part-1-excercise/part1/src/App.jsx
@@ -204,31 +204,41 @@
 //************************************************************************* */
 
 //Chapter 1 Part D Excercise 1.6
-
-import { useState } from 'react'
+import React, { useState } from 'react';
 
 const App = () => {
   // save clicks of each button to its own state
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const handleGoodClick = () => {
+    setGood(good + 1);
+  };
+
+  const handleNeutralClick = () => {
+    setNeutral(neutral + 1);
+  };
+
+  const handleBadClick = () => {
+    setBad(bad + 1);
+  };
 
   return (
     <div>
       <h1>give feedback</h1>
-      <button>good</button> 
-      <button>nuetral</button>
-      <button>bad</button>
+      <button id="buttonone" onClick={handleGoodClick}>good</button>
+      <button id="buttontwo" onClick={handleNeutralClick}>neutral</button>
+      <button id="buttonthree" onClick={handleBadClick}>bad</button>
       <h2>Statistics</h2>
-      <p>good 6</p>
-      <p>nuetral 2</p>
-      <p>bad 1</p>
-      
+      <p>Good: {good}</p>
+      <p>Neutral: {neutral}</p>
+      <p>Bad: {bad}</p>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
 
 //************************************************************************ */
 //Excercise 4.1
